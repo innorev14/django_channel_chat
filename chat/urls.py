ba09cdfcbfec299c6ffe13d4f69fdf7f0fc99e66
innorev14 <tokyo3@netsgo.com>
2020-05-22 18:18:43 +0900

    feat/ add chat room

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, room
 
-urlpatterns = {
-    path('', views.index, name='index')
-}
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+]
